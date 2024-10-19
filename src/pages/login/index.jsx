a3250f8a3369ddae8fa5db19ceada8b68c98f436
luizmiguelsoares <luiz.miguel1406007@gmail>
2024-10-19 15:34:25 -0300

    primeira api

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import './index.scss'
+import axios from 'axios';
 
 export default function Login  (){
+
+const [senha,setsenha] = useState('')
+const [usuario,setusuario ] = useState('')
+
+    async function salvar() {
+        const id = {
+            "usuario" :usuario,
+            "senha" : senha
+        }
+
+        const url = 'http://localhost:5010/usuario';
+        let resp = await axios.post(url, id);
+
+        alert('usuario adicionado. Id: ' + resp.data.novoId);
+    }
+
 
     return(
         <div className="login">
@@ -11,9 +29,9 @@ export default function Login  (){
             <p>Você esta  prestes a entrar  em modo adimin!</p></div>
              <div className='cont2'>
                 <h1>Login Admin</h1>
-            <input type="text" placeholder="Usuario:" />
-            <input type="text" placeholder="Senha:" />
-            <button className='continuar'>Continuar</button>
+            <input value={usuario} onChange={e =>setusuario(e.target.value)}   type="text" placeholder="Usuario:" />
+            <input  value={senha} onChange={e =>setsenha(e.target.value)}    type="text" placeholder="Senha:" />
+            <button onClick={salvar} className='continuar'>Continuar</button>
            
             </div>
           </div>
